58527a50c5a2f219d024fe0de88581a5e004ef86
belangerpascal <54826336+belangerpascal@users.noreply.github.com>
2024-03-10 17:50:22 -0400

    up

--- app/disp.py
+++ app/disp.py
@@ -1,11 +1,10 @@
-from collections import deque
 import sys
 import psutil
 import board
 import digitalio
+import pygame
 from adafruit_rgb_display.st7789 import ST7789
-import matplotlib.pyplot as plt
-from PIL import Image
+from collections import deque
 from gpiozero import Device
 
 # User Config
@@ -16,17 +15,17 @@ PLOT_CONFIG = [
         'title': 'LOAD',
         'ylim': (0, 100),
         'line_config': [
-            {'color': '#0000FF', 'width': 2},
-            {'color': '#0060FF', 'width': 2},
-            {'color': '#00FF60', 'width': 2},
-            {'color': '#60FF00', 'width': 2},
+            {'color': (0, 0, 255), 'width': 2},
+            {'color': (0, 96, 255), 'width': 2},
+            {'color': (0, 255, 96), 'width': 2},
+            {'color': (96, 255, 0), 'width': 2},
         ]
     },
     {
         'title': 'TEMP',
         'ylim': (20, 50),
         'line_config': [
-            {'color': '#FF0000', 'width': 2},
+            {'color': (255, 0, 0), 'width': 2},
         ]
     }
 ]
@@ -46,50 +45,28 @@ y_data = [
 # Setup display
 disp = ST7789(
     board.SPI(),
-    height=280,
-    width=240,
+    height=240,
+    width=280,
     y_offset=20,
-    rotation=0,
+    rotation=90,
     baudrate=40000000,
     cs=digitalio.DigitalInOut(board.CE0),
     dc=digitalio.DigitalInOut(board.D25),
     rst=digitalio.DigitalInOut(board.D27)
 )
 
-# Setup plot figure
-plt.style.use('dark_background')
-fig, ax = plt.subplots(2, 1, figsize=(disp.width / 100, disp.height / 100))
+# Initialize pygame
+pygame.init()
 
-# Setup plot axis
-ax[0].xaxis.set_ticklabels([])
-for plot, a in enumerate(ax):
-    # Add grid to all plots
-    a.grid(True, linestyle=':')
-    # Limit and invert x time axis
-    a.set_xlim(min(x_time), max(x_time))
-    a.invert_xaxis()
-    # Custom settings
-    if 'title' in PLOT_CONFIG[plot]:
-        a.set_title(PLOT_CONFIG[plot]['title'], position=(0.5, 0.8))
-    if 'ylim' in PLOT_CONFIG[plot]:
-        a.set_ylim(PLOT_CONFIG[plot]['ylim'])
+# Create a pygame window
+window = pygame.display.set_mode((disp.width, disp.height))
 
-# Setup plot lines
+# Set up plot lines
 plot_lines = []
 for plot, config in enumerate(PLOT_CONFIG):
     lines = []
     for index, line_config in enumerate(config['line_config']):
-        # create line
-        line, = ax[plot].plot(x_time, y_data[plot][index])
-        # custom settings
-        if 'color' in line_config:
-            line.set_color(line_config['color'])
-        if 'width' in line_config:
-            line.set_linewidth(line_config['width'])
-        if 'style' in line_config:
-            line.set_linestyle(line_config['style'])
-        # add line to list
-        lines.append(line)
+        lines.append(pygame.draw.aaline(window, line_config['color'], (0, 0), (0, 0), line_config['width']))
     plot_lines.append(lines)
 
 def update_data():
@@ -117,45 +94,28 @@ def update_plot():
     # update lines with the latest data
     for plot, lines in enumerate(plot_lines):
         for index, line in enumerate(lines):
-            line.set_ydata(y_data[plot][index])
-    # draw the plots
-    canvas = plt.get_current_fig_manager().canvas
-    plt.tight_layout()
-    canvas.draw()
-
-    # force a redraw of the entire figure
-    plt.draw()
-
-    # transfer into PIL image
-    original_image = Image.frombytes('RGBA', canvas.get_width_height(), canvas.buffer_rgba())
-
-    # Create a new image with the exact display dimensions
-    new_image = Image.new('RGBA', (disp.width, disp.height), (0, 0, 0, 255))
-    new_image.paste(original_image.crop((0, 0, disp.width, disp.height)), (0, 0))
-
-    disp.image(new_image)
-
-    # update the plot after displaying the image
-    for plot, lines in enumerate(plot_lines):
-        for index, line in enumerate(lines):
-            line.set_ydata(y_data[plot][index])
-        # autoscale if not specified
-        if 'ylim' not in PLOT_CONFIG[plot].keys():
-            ax[plot].relim()
-            ax[plot].autoscale_view()
-    print("Plot Updated")
-
+            data = y_data[plot][index]
+            if None not in data:
+                avg_data = sum(data) / len(data)
+                pygame.draw.aaline(window, PLOT_CONFIG[plot]['line_config'][index]['color'],
+                                   (x_time[0], avg_data), (x_time[-1], avg_data),
+                                   PLOT_CONFIG[plot]['line_config'][index]['width'])
+    pygame.display.flip()
 
 try:
     print("Looping")
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
         update_data()
         update_plot()
         sys.stdout.flush()
-        plt.pause(0.05)
 except KeyboardInterrupt:
     print("Loop interrupted by user.")
 finally:
+    pygame.quit()
     if Device.pin_factory is not None:
         Device.pin_factory.reset()
     print("Exiting program.")
