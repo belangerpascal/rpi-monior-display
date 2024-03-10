5ab9f8b86fd3c3efcb13e582a4d995f2d1da6530
belangerpascal <54826336+belangerpascal@users.noreply.github.com>
2024-03-10 18:46:46 -0400

    up

--- app/disp.py
+++ app/disp.py
@@ -1,11 +1,11 @@
+from collections import deque
 import sys
 import psutil
 import board
 import digitalio
-import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw
 from adafruit_rgb_display.st7789 import ST7789
-from collections import deque
+import matplotlib.pyplot as plt
+from PIL import Image
 from gpiozero import Device
 
 # User Config
@@ -16,22 +16,22 @@ PLOT_CONFIG = [
         'title': 'LOAD',
         'ylim': (0, 100),
         'line_config': [
-            {'color': (0, 0, 255), 'width': 2},
-            {'color': (0, 96, 255), 'width': 2},
-            {'color': (0, 255, 96), 'width': 2},
-            {'color': (96, 255, 0), 'width': 2},
+            {'color': '#0000FF', 'width': 2},
+            {'color': '#0060FF', 'width': 2},
+            {'color': '#00FF60', 'width': 2},
+            {'color': '#60FF00', 'width': 2},
         ]
     },
     {
         'title': 'TEMP',
         'ylim': (20, 50),
         'line_config': [
-            {'color': (255, 0, 0), 'width': 2},
+            {'color': '#FF0000', 'width': 2},
         ]
     }
 ]
 
-CPU_COUNT = len(psutil.cpu_percent(interval=REFRESH_RATE, percpu=True))
+CPU_COUNT = 1
 
 # Setup X data storage
 x_time = [x * REFRESH_RATE for x in range(HIST_SIZE)]
@@ -56,9 +56,41 @@ disp = ST7789(
     rst=digitalio.DigitalInOut(board.D27)
 )
 
-# Create an image buffer
-image = Image.new("RGB", (disp.width, disp.height))
-draw = ImageDraw.Draw(image)
+# Setup plot figure
+plt.style.use('dark_background')
+fig, ax = plt.subplots(2, 1, figsize=(disp.width / 100, disp.height / 100))
+
+# Setup plot axis
+ax[0].xaxis.set_ticklabels([])
+for plot, a in enumerate(ax):
+    # Add grid to all plots
+    a.grid(True, linestyle=':')
+    # Limit and invert x time axis
+    a.set_xlim(min(x_time), max(x_time))
+    a.invert_xaxis()
+    # Custom settings
+    if 'title' in PLOT_CONFIG[plot]:
+        a.set_title(PLOT_CONFIG[plot]['title'], position=(0.5, 0.8))
+    if 'ylim' in PLOT_CONFIG[plot]:
+        a.set_ylim(PLOT_CONFIG[plot]['ylim'])
+
+# Setup plot lines
+plot_lines = []
+for plot, config in enumerate(PLOT_CONFIG):
+    lines = []
+    for index, line_config in enumerate(config['line_config']):
+        # create line
+        line, = ax[plot].plot(x_time, y_data[plot][index])
+        # custom settings
+        if 'color' in line_config:
+            line.set_color(line_config['color'])
+        if 'width' in line_config:
+            line.set_linewidth(line_config['width'])
+        if 'style' in line_config:
+            line.set_linestyle(line_config['style'])
+        # add line to list
+        lines.append(line)
+    plot_lines.append(lines)
 
 def update_data():
     # Update data
@@ -76,42 +108,42 @@ def update_data():
     for plot, limits in enumerate(PLOT_CONFIG):
         if 'ylim' in limits:
             for index, data_point in enumerate(cpu_percs if plot == 0 else cpu_temps):
-                limit_min, limit_max = limits['ylim']
-                if not (limit_min <= float(data_point) <= limit_max):
+                if not limits['ylim'][0] <= data_point <= limits['ylim'][1]:
                     print(f"Warning: Data point {data_point} is outside the y-axis limits for Plot {plot + 1}, Line {index + 1}")
-                    print(f"Y-axis limits: {limit_min} to {limit_max}")
+
+    sys.stdout.flush()
+
 
 def update_plot():
-    # Clear the image
-    draw.rectangle([(0, 0), image.size], fill=(0, 0, 0))
+    # update lines with latest data
+    for plot, lines in enumerate(plot_lines):
+        for index, line in enumerate(lines):
+            line.set_ydata(y_data[plot][index])
+        # autoscale if not specified
+        if 'ylim' not in PLOT_CONFIG[plot].keys():
+            ax[plot].relim()
+            ax[plot].autoscale_view()
+    # draw the plots
+    canvas = plt.get_current_fig_manager().canvas
+    plt.tight_layout()
+    canvas.draw()
+    # transfer into PIL image and display
+    image = Image.frombytes('RGBA', canvas.get_width_height(), canvas.buffer_rgba())
+    print("Plot Updated")
+    print(f"Image Dimensions: {image.size}")
+    sys.stdout.flush()  # Flush the output buffer
+    disp.image(image)
 
-    # Update lines with the latest data
-    for plot, config in enumerate(PLOT_CONFIG):
-        for index, line_config in enumerate(config['line_config']):
-            data = y_data[plot][index]
-            if None not in data:
-                avg_data = sum(data) / len(data)
-                color = line_config['color']
-                width = line_config['width']
-                points = [(x_time[i], avg_data) for i in range(len(x_time))]
-                draw.line(points, fill=color, width=width)
-
-    # Create a new image with the correct dimensions
-    new_image = Image.new('RGB', (disp.width, disp.height), (0, 0, 0))
-    new_image.paste(image, (20, 0))  # Paste the original image with an offset
-
-    # Display the updated image
-    disp.image(new_image)
-
-
-
+MAX_ITERATIONS = 1000
+iteration_count = 0
 
 try:
     print("Looping")
-    while True:
+    while iteration_count < MAX_ITERATIONS:
         update_data()
         update_plot()
-        sys.stdout.flush()
+        print(iteration_count)
+        iteration_count += 1
 except KeyboardInterrupt:
     print("Loop interrupted by user.")
 finally:
