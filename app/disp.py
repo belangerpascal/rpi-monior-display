0bb53f062c51a7087964898b46d75bec2c19281d
belangerpascal <54826336+belangerpascal@users.noreply.github.com>
2024-03-23 11:32:18 -0400

    Update disp.py

--- app/disp.py
+++ app/disp.py
@@ -3,29 +3,31 @@ import sys
 import psutil
 import board
 import digitalio
-from adafruit_rgb_display.st7789 import ST7789
+from adafruit_rgb_display import st7789
+#from adafruit_rgb_display.st7789 import ST7789
 import matplotlib.pyplot as plt
 from PIL import Image
 from gpiozero import Device
 import time
 
 # User Config
-REFRESH_RATE = 0.05
+REFRESH_RATE = 0.10
 HIST_SIZE = 61
+px = 1/plt.rcParams['figure.dpi']
 PLOT_CONFIG_CPU = [
     {
         'title': 'LOAD',
         'ylim': (0, 100),
         'line_config': [
-            {'color': '#0000FF', 'width': 2},
-            {'color': '#0060FF', 'width': 2},
-            {'color': '#00FF60', 'width': 2},
-            {'color': '#60FF00', 'width': 2},
+            {'color': '#0000FF', 'width': .5},
+            {'color': '#0060FF', 'width': .5},
+            {'color': '#00FF60', 'width': .5},
+            {'color': '#60FF00', 'width': .5},
         ]
     }
 ]
 
-CPU_COUNT = 1
+CPU_COUNT = 4
 
 # Setup X data storage
 x_time = [x * REFRESH_RATE for x in range(HIST_SIZE)]
@@ -38,47 +40,34 @@ y_data = [
 ]
 
 # Setup display
-disp = ST7789(
-    board.SPI(),
-    height=280,
-    width=240,
-    y_offset=20,
-    rotation=0,
-    baudrate=40000000,
-    cs=digitalio.DigitalInOut(board.CE0),
-    dc=digitalio.DigitalInOut(board.D25),
-    rst=digitalio.DigitalInOut(board.D27)
-)
+#disp = ST7789(
+#    board.SPI(),
+#    height=280,
+#    width=240,
+#    y_offset=20,
+#    rotation=0,
+#    baudrate=40000000,
+#    cs=digitalio.DigitalInOut(board.CE0),
+#    c=digitalio.DigitalInOut(board.D25),
+#    rst=digitalio.DigitalInOut(board.D27)
+#)
+disp = st7789.ST7789(board.SPI(), height=280, width=280, y_offset=20, rotation=90,
+                     baudrate=40000000,
+                     cs=digitalio.DigitalInOut(board.CE0),
+                     dc=digitalio.DigitalInOut(board.D25),
+                     rst=digitalio.DigitalInOut(board.D27))
 
 # Setup plot figure
 plt.style.use('dark_background')
-# fig, ax = plt.subplots(1, 1, figsize=(disp.width / 100, disp.height / 100))
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(280*px, 240*px))
 
-# Setup plot axis
 ax.xaxis.set_ticklabels([])
-#for plot, a in enumerate(ax):
-#    # Add grid to all plots
-#    a.grid(True, linestyle=':')
-#    # Limit and invert x time axis
-#    a.set_xlim(min(x_time), max(x_time))
-#    a.invert_xaxis()
-#    # Custom settings
-#    if 'title' in PLOT_CONFIG_CPU[plot]:
-#        a.set_title(PLOT_CONFIG_CPU[plot]['title'], position=(0.5, 0.8))
-#    if 'ylim' in PLOT_CONFIG_CPU[plot]:
-#        a.set_ylim(PLOT_CONFIG_CPU[plot]['ylim'])
-
-# Add grid to all plots
-ax.grid(True, linestyle=':')
-# Limit and invert x time axis
+#ax.grid(True, linestyle=':')
 ax.set_xlim(min(x_time), max(x_time))
 ax.invert_xaxis()
 # Custom settings
-if 'title' in PLOT_CONFIG_CPU:
-    ax.set_title(PLOT_CONFIG_CPU['title'], position=(0.5, 0.8))
-if 'ylim' in PLOT_CONFIG_CPU:
-    ax.set_ylim(PLOT_CONFIG_CPU['ylim'])
+ax.set_title("LOAD", position=(0.5, 0.8))
+ax.set_ylim((0, 100))
 
 # Setup plot lines
 plot_lines = []
@@ -86,7 +75,8 @@ for plot, config in enumerate(PLOT_CONFIG_CPU):
     lines = []
     for index, line_config in enumerate(config['line_config']):
         # create line
-        line, = ax[plot].plot(x_time, y_data[plot][index])
+        #line, = ax[plot].plot(x_time, y_data[plot][index])
+        line, = ax.plot(x_time, y_data[plot][index])
         # custom settings
         if 'color' in line_config:
             line.set_color(line_config['color'])
@@ -101,13 +91,14 @@ for plot, config in enumerate(PLOT_CONFIG_CPU):
 def update_data():
     # Update data
     cpu_percs = psutil.cpu_percent(interval=REFRESH_RATE, percpu=True)
-    y_data[0].append(cpu_percs)
+    for cpu in range(CPU_COUNT):
+        y_data[0][cpu].append(cpu_percs[cpu])
 
     # cpu_temps = [shwtemp.current for shwtemp in psutil.sensors_temperatures().get('cpu_thermal', [])]
     # y_data[1].append(cpu_temps)
 
     # Print statements for debugging
-    print(f"CPU Percentages: {cpu_percs}")
+    # print(f"CPU Percentages: {cpu_percs}")
     # print(f"CPU Temperatures: {cpu_temps}")
 
 def update_plot():
@@ -116,7 +107,7 @@ def update_plot():
         for index, line in enumerate(lines):
             line.set_ydata(y_data[plot][index])
         # autoscale if not specified
-        if 'ylim' not in PLOT_CONFIG[plot].keys():
+        if 'ylim' not in PLOT_CONFIG_CPU[plot].keys():
             ax[plot].relim()
             ax[plot].autoscale_view()
     # draw the plots
@@ -128,14 +119,17 @@ def update_plot():
                             canvas.tostring_rgb())
     disp.image(image)
 
-try:
-    print("Looping")
-    while iteration_count < MAX_ITERATIONS:
-        update_data()
-        update_plot()
-except KeyboardInterrupt:
-    print("Loop interrupted by user.")
-finally:
-    if Device.pin_factory is not None:
-        Device.pin_factory.reset()
-    print("Exiting program.")
+#try:
+#    print("Looping")
+#    update_data()
+#    update_plot()
+#except KeyboardInterrupt:
+#    print("Loop interrupted by user.")
+#finally:
+#    if Device.pin_factory is not None:
+#        Device.pin_factory.reset()
+#    print("Exiting program.")
+print("looping")
+while True:
+    update_data()
+    update_plot()
